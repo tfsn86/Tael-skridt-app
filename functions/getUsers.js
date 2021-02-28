b8a71d43671c3708ce5a52429e9dd3d35f66f749
tfsn <tfsn86@gmail.com>
2021-02-28 18:45:56 +0100

    Update getUsers.js

--- functions/getUsers.js
+++ functions/getUsers.js
@@ -1,8 +1,7 @@
 exports.handler = function (event, context, callback) {
-	const { name } = JSON.parse(event.body);
-
-	callback(null, {
-		statusCode: 200,
-		body: `Hello ${name}`,
-	});
+	// const { name } = JSON.parse(event.body);
+	// callback(null, {
+	// 	statusCode: 200,
+	// 	body: `Hello ${name}`,
+	// });
 };
